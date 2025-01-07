b0bb4a145c766cfc64a2e230639a96b3c032b608
Atishfulzade <atishfulzade1234@gmail.com>
2025-01-07 16:12:34 +0530

    added color

--- src/components/TaskManagement.jsx
+++ src/components/TaskManagement.jsx
@@ -51,7 +51,7 @@ const TaskManagementApp = ({ tasks, setTasks }) => {
             </div>
             {tasks.some((task) => task.completed) && (
               <button
-                className=" px-3 py-2  border   rounded"
+                className=" px-3 py-2 hover:bg-slate-100 border rounded"
                 onClick={deleteSlectedTask}
               >
                 Delete selected task
